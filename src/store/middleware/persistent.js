933b780afe0eb29a1ccc6d9e2bab6eba8bc560f7
Jdeshz <17858801330@163.com>
2019-06-10 09:39:31 +0800

    format

--- src/store/middleware/persistent.js
+++ src/store/middleware/persistent.js
@@ -1,19 +1,19 @@
 /**
  * 持久化存储 state 数据
- * 
+ *
  * 主要将数据存储进sessionStorage
  */
 
 const persistent = store => next => action => {
-    var result = next(action)
-    var data = store.getState()
-    for (var key in data) {
-        var v = data[key]
-        if (typeof (data[key]) === 'object') v = JSON.stringify(v)
-        sessionStorage.setItem(key, v)
-    }
-    console.log('The data has been stored')
-    return result
+  var result = next(action)
+  var data = store.getState()
+  for (var key in data) {
+    var v = data[key]
+    if (typeof data[key] === 'object') v = JSON.stringify(v)
+    sessionStorage.setItem(key, v)
+  }
+  console.log('The data has been stored')
+  return result
 }
 
 export default persistent
