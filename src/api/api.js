832c02c9f3f0df59fce2b72bc7de7886a3338484
Jdeshz <17858801330@163.com>
2019-06-10 11:11:45 +0800

    axios 请求队列

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,6 @@ import axios from 'axios'
 import { message } from 'antd'
 
 import store from '../store/store'
-
 import ACTIONTYPE from '../store/action.types'
 
 /**
@@ -14,25 +13,50 @@ import ACTIONTYPE from '../store/action.types'
  */
 message.config({
   duration: 2,
-  maxCount: 2,
+  maxCount: 2
 })
-
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '/api' : ''
 
+const pending = [] //请求队列
+const cancelToken = axios.CancelToken //axios abort
+
+/**
+ * 辅助函数,移除已经完成的请求
+ */
+const removePending = ev => {
+  var uid = ev.data ? JSON.stringify(ev.data) : 'undefined'
+  console.log(uid)
+  var index = pending.findIndex(row => {
+    return row.url === ev.url && uid === row.uid
+  })
+  if (index > -1) {
+    pending[index].cancel()
+    pending.splice(index, 1)
+  }
+}
 
 // http request 拦截器
-axios.interceptors.request.use(config => {
-  if (store.getState().token) { // 判断是否存在token，如果存在的话，则每个http header都加上token
-    config.headers.Authorization = store.getState().token
-  }
-  return config
-},
+axios.interceptors.request.use(
+  config => {
+    removePending(config) //移除重复的请求
+    //部署 abort
+    var uid = config.data ? JSON.stringify(config.data) : 'undefined'
+    config.cancelToken = new cancelToken(fn => {
+      pending.push({ url: config.url, cancel: fn, uid: uid })
+    })
+    console.log('pending', pending)
+    if (store.getState().token) {
+      // 判断是否存在token，如果存在的话，则每个http header都加上token
+      config.headers.Authorization = store.getState().token
+    }
+    return config
+  },
   err => {
     message.warning(err)
     return Promise.reject(err)
-  })
-
+  }
+)
 
 // http response 拦截器
 axios.interceptors.response.use(
@@ -45,6 +69,7 @@ axios.interceptors.response.use(
     } else {
       message.warn(response.data.msg)
     }
+    removePending(response.config) //请求成功则移出请求队列
     return response
   },
   error => {
@@ -61,9 +86,16 @@ axios.interceptors.response.use(
           break
       }
       //将错误信息 扔进redux 触发路由重定向
-      store.dispatch({ type: ACTIONTYPE.http, payload: { status: error.response.status, msg: error.response.statusText } })
+      store.dispatch({
+        type: ACTIONTYPE.http,
+        payload: {
+          status: error.response.status,
+          msg: error.response.statusText
+        }
+      })
     }
     return Promise.reject(error.response.data) // 返回接口返回的错误信息
-  })
+  }
+)
 
 export default axios
--- src/utils/tool.js
+++ src/utils/tool.js
@@ -2,17 +2,16 @@
  * tool工具类 by jdes on 2019-04-23
  */
 
-
 /**
  * 将 树形结构 转化 为 List
  */
 export function __ToList(data) {
-    var result = []
-    data.forEach(row => {
-        if (row.children && row.children.length > 0) return __ToList(row.children)
-        result.push(row)
-    })
-    return result
+  var result = []
+  data.forEach(row => {
+    if (row.children && row.children.length > 0) return __ToList(row.children)
+    result.push(row)
+  })
+  return result
 }
 
 /**
@@ -20,38 +19,37 @@ export function __ToList(data) {
  */
 
 export function __ToTree(data, perKey) {
-    var map = {}
-    /**
-     * 预处理,生成一个以id为主键的map便于后续查找
-     */
-    data.forEach(item => {
-        map[item.id] = item
-    })
+  var map = {}
+  /**
+   * 预处理,生成一个以id为主键的map便于后续查找
+   */
+  data.forEach(item => {
+    map[item.id] = item
+  })
 
-    /**
-     * 生成tree结构
-     */
+  /**
+   * 生成tree结构
+   */
 
-    var result = []
+  var result = []
 
-    data.forEach(row => {
-        var parent = map[row[perKey]] //查找当前数据的pid,即父元素在不在，如果在证明他是子集，扔进该元素的children中即可，否则扔进顶级
-        if (parent) {
-            (parent.children || (parent.children = [])).push(row)
-        } else {
-            result.push(row)
-        }
-    })
-    return result
+  data.forEach(row => {
+    var parent = map[row[perKey]] //查找当前数据的pid,即父元素在不在，如果在证明他是子集，扔进该元素的children中即可，否则扔进顶级
+    if (parent) {
+      ;(parent.children || (parent.children = [])).push(row)
+    } else {
+      result.push(row)
+    }
+  })
+  return result
 }
 
-
 /**
- * 添加 0 
+ * 添加 0
  */
 
 export function __num(num) {
-    return num < 10 ? '0' + num : num
+  return num < 10 ? '0' + num : num
 }
 
 /**
@@ -59,25 +57,25 @@ export function __num(num) {
  */
 
 export function __Time(date) {
-    let Y = date.getFullYear() // 年
-    let m = __num(date.getMonth() + 1) // 月
-    let d = __num(date.getDate()) // 日
-    let h = __num(date.getHours()) // 时
-    let i = __num(date.getMinutes()) // 分
-    let s = __num(date.getSeconds()) // 秒
-    return Y + '-' + m + '-' + d + ' ' + h + ':' + i + ':' + s
+  let Y = date.getFullYear() // 年
+  let m = __num(date.getMonth() + 1) // 月
+  let d = __num(date.getDate()) // 日
+  let h = __num(date.getHours()) // 时
+  let i = __num(date.getMinutes()) // 分
+  let s = __num(date.getSeconds()) // 秒
+  return Y + '-' + m + '-' + d + ' ' + h + ':' + i + ':' + s
 }
 
 /**
  * 单次执行函数
- * @param {传入的函数} fn 
+ * @param {传入的函数} fn
  */
 
 export const __Once = fn => {
-    let done = false
-    return function () {
-        return done ? undefined : ((done = true), fn.apply(this, arguments))
-    }
+  let done = false
+  return function() {
+    return done ? undefined : ((done = true), fn.apply(this, arguments))
+  }
 }
 
 /**
@@ -87,15 +85,15 @@ export const __Once = fn => {
  * @returns {Function}
  */
 export function __Debounce(method, delay = 500) {
-    let timer = null;
-    return function () {
-        let self = this,
-            args = arguments
-        timer && clearTimeout(timer)
-        timer = setTimeout(function () {
-            method.apply(self, args)
-        }, delay)
-    }
+  let timer = null
+  return function() {
+    let self = this,
+      args = arguments
+    timer && clearTimeout(timer)
+    timer = setTimeout(function() {
+      method.apply(self, args)
+    }, delay)
+  }
 }
 
 /**
@@ -104,25 +102,36 @@ export function __Debounce(method, delay = 500) {
  * @param mustRunDelay 间隔多少毫秒需要触发一次事件
  */
 export function __Throttle(method, mustRunDelay = 500) {
-    let timer,
-        args = arguments,
-        start
-    return function loop() {
-        let self = this
-        let now = Date.now()
-        if (!start) {
-            start = now
-        }
-        if (timer) {
-            clearTimeout(timer)
-        }
-        if (now - start >= mustRunDelay) {
-            method.apply(self, args);
-            start = now
-        } else {
-            timer = setTimeout(function () {
-                loop.apply(self, args)
-            }, 50)
-        }
+  let timer,
+    args = arguments,
+    start
+  return function loop() {
+    let self = this
+    let now = Date.now()
+    if (!start) {
+      start = now
     }
+    if (timer) {
+      clearTimeout(timer)
+    }
+    if (now - start >= mustRunDelay) {
+      method.apply(self, args)
+      start = now
+    } else {
+      timer = setTimeout(function() {
+        loop.apply(self, args)
+      }, 50)
+    }
+  }
+}
+
+/**
+ * GUID
+ */
+export function __guid() {
+  return 'xxxxxxxxxxxxxxxyxxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }
